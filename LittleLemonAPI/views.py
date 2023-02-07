05e65732b9ee75f61c45b3e089e3e2ad05049cbc
naeem252 <naeemhasan252@gmail.com>
2023-02-07 20:38:49 +0600

    pagination

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.decorators import api_view
 from .models import MenuItem,FastFood,Category
+from django.core.paginator import Paginator,EmptyPage
 from .serializers import MenuItemSerializer,FastFoodSerializer,CategorySerializer
 
 # views using function
@@ -15,6 +16,8 @@ def menu_items(request):
         to_price = request.query_params.get("to_price")
         search = request.query_params.get("search")
         ordering = request.query_params.get("ordering")
+        perpage = request.query_params.get("perpage",default=2)
+        page = request.query_params.get('page',default=1)
         if ordering:
             ordering_list = ordering.split(",")
             items = items.order_by(*ordering_list)
@@ -24,6 +27,11 @@ def menu_items(request):
             items = items.filter(category__title=category_name)
         if to_price:
             items = items.filter(price__lte=to_price)
+        paginator = Paginator(items,per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except EmptyPage:
+            items = []
         serialize_item = MenuItemSerializer(items,many=True)
         return Response(serialize_item.data)
     elif request.method == 'POST':
