0361e0bb79fb7247beebc8d38d3a94e511d9b4a7
naeem252 <naeemhasan252@gmail.com>
2023-02-07 19:13:11 +0600

    sanitization using bleach module

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import MenuItem,FastFood,Category
 from decimal import Decimal
+import bleach
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,11 +9,27 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','slug','title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    # category = CategorySerializer()
-    # category = serializers.RelatedField()
-    class Meta:
-        model = MenuItem
-        fields = ['id','title','price','inventory','category']
+  stock = serializers.IntegerField(source='inventory')
+  price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
+  category_id = serializers.IntegerField(write_only=True)
+  category = CategorySerializer(read_only=True)
+  def validate_title(self,value):
+     return bleach.clean(value)
+  def validate_price(self,value):
+        if (value < 2):
+         raise serializers.ValidationError("Price should not be less than 2.0")
+        return value
+        
+  class Meta:
+   model = MenuItem
+   fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
+   extra_kwargs = {
+      'stock':{'source':'inventory','min_value':0}
+   }
+   
+      
+  def calculate_tax(self, product:MenuItem):
+   return product.price * Decimal(1.1)
         
 
 class FastFoodSerializer(serializers.ModelSerializer):
